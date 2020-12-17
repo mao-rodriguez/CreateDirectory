b6c7d60c35a62b9c9d585409d9d052e7ad7b828b
marr4631 <mauricio.rodriguez@christus.co>
2020-12-17 10:16:39 -0500

    Add project files.

---
+++ CreateDirectory/CheckPath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CreateDirectory
+{
+    class Folder
+    {
+        public static bool CheckPath(string path)
+        {
+            char[] invalidPathChars = Path.GetInvalidFileNameChars();
+            foreach (char c in path)
+            {
+                foreach (char invalidChar in invalidPathChars)
+                {
+                    bool wrongChar = c.Equals(invalidChar);
+                    if (wrongChar)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CreateDirectory/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace CreateDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the directory to manipulate
+            //string path = @"c:\testDir\secondTest";
+            string path = Console.ReadLine();
+
+            try
+            {
+                
+                // Determine whether the directory exists.
+                if (Directory.Exists(path))
+                {
+                    Console.WriteLine("The path already exists");
+                    return;
+                }
+
+                if (Folder.CheckPath(path))
+                {
+                    string invalidCharacters = new string(Path.GetInvalidPathChars());
+                    Console.WriteLine("Folder names cannot have any of next characters " + invalidCharacters);
+                    return;
+                }
+
+                // Try to create the directory.
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                Console.WriteLine("The directory was created successfuly at {0}.", Directory.GetCreationTime(path));
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("The process failed at {0}", e.ToString());
+            }
+
+            finally { }
+
+        }
+        
+        //public bool CheckPath(string path)
+        //{
+        //    char[] invalidPathChars = Path.GetInvalidPathChars();
+        //    foreach(char c in path)
+        //    {
+        //        foreach(char invalidChar in invalidPathChars)
+        //        {
+        //            bool wrongChar = c.Equals(invalidChar);
+        //            if (wrongChar) 
+        //            {
+        //                return true;
+        //            }
+        //        }
+        //    }
+        //    return false;
+        //}
+        
+    }
+}
